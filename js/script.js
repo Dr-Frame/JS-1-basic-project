a21766a73dcdd5fe9e170f24a72c810e82e2ed14
Vlad Dr.Frame <remenjuk2010@gmail.com>
2021-03-02 07:20:24 +0200

    30 done

--- js/script.js
+++ js/script.js
@@ -12,15 +12,54 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
-'use strict';
+"use strict";
 
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+  movies: [
+    "Логан",
+    "Лига справедливости",
+    "Ла-ла лэнд",
+    "Одержимость",
+    "Скотт Пилигрим против...",
+    "Игра пристолов",
+  ],
 };
 
+//1)
+const addBlocksRef = document.querySelectorAll(".promo__adv img");
+addBlocksRef.forEach((item) => item.remove());
+
+//2)
+const genre = document.querySelector(".promo__genre");
+genre.textContent = "драма";
+
+// 3)
+const promoBgRef = document.querySelector(".promo__bg");
+promoBgRef.style.backgroundImage = "url(../img/bg.jpg)";
+
+/* promoBgRef.style.background =
+  "url(../img/bg.jpg) center center/cover no-repeat"; */
+
+// 4,5))
+movieDB.movies.sort();
+
+// ==== вариант если всегда показывает только 5 фильмов на странице ===============
+
+/* const listItemRef = document.querySelectorAll(".promo__interactive-item");
+
+listItemRef.forEach((item, i) => {
+  item.textContent = `${i + 1}. ${movieDB.movies[i]}`;
+}); */
+
+// === вариант если рендерим страницу где больше чем 5 вариантов может быть ==============
+
+const listItemRef = document.querySelector(".promo__interactive-list");
+listItemRef.innerHTML = "";
+
+movieDB.movies.forEach((film, i) => {
+  const toInsert = `<li class="promo__interactive-item">${
+    i + 1
+  }. ${film}<div class="delete"></div></li>`;
+
+  listItemRef.insertAdjacentHTML("beforeend", toInsert);
+});
